a4b53670943c1bfb301ab8d12241d5125987f4f5
arnavhax <113230128+arnavhax@users.noreply.github.com>
2023-09-15 06:16:54 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+participants = [
+    {
+        'id': 1,
+        'name': "Arnav Balpande",
+        'rank': 1,
+        'score':50,
+    },
+    {
+        'id': 2,
+        'name': "Riya Kashikar",
+        'rank': 2,
+        'score':10,
+    },
+]
+
 
 @app.route('/')
 def home():
-  return render_template('home.html')
+  return render_template('home.html', participants=participants)
 
-if __name__=="__main__":
-  app.run(host='0.0.0.0',debug=True)
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
