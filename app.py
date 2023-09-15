d69d1be8091ba26cb4695a69de4c40372730b209
arnavhax <113230128+arnavhax@users.noreply.github.com>
2023-09-15 18:20:05 +0000

    removed bs4 and used api to fetch data
    never trust bs4

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template
 import requests
-from bs4 import BeautifulSoup
+import json
 
 profileLinks = [
-    "https://leetcode.com/arnavhax/", "https://leetcode.com/Anirudha_Shekdar/",
-    "https://leetcode.com/BeNeatCo/", "https://leetcode.com/hazsyl1/",
-    "https://leetcode.com/dulhaniaa/", "https://leetcode.com/yuvrajzanwar/",
-    "https://leetcode.com/shrivastavapa/",
-    "https://leetcode.com/prathameshratthe/",
-    "https://leetcode.com/jatinbaheti/",
-    "https://leetcode.com/Priyanshi_0912/", "https://leetcode.com/aalok2025/"
+    "arnavhax", "Anirudha_Shekdar",
+    "BeNeatCo", "hazsyl1",
+    "dulhaniaa", "yuvrajzanwar",
+    "shrivastavapa",
+    "prathameshratthe",
+    "jatinbaheti",
+    "Priyanshi_0912","aalok2025"
 ]
 
 
@@ -17,33 +17,23 @@ def makeParticipants(profileLinks):
   participants = []
   for k in profileLinks:
     entity = {}
+    url="https://leetcode-stats-api.herokuapp.com/"+k
     username = ''
     score = 0
-    response = requests.get(k)
-    soup = BeautifulSoup(response.content, "html.parser")
+    response = requests.get(url)
     entity = {}
-    questions = soup.findAll(
-        'span',
-        class_=
-        'mr-[5px] text-base font-medium leading-[20px] text-label-1 dark:text-dark-label-1'
-    )
-    for i in range(len(questions)):
-      if (questions[i] is not None):
-        if (i == 0):
-          score += int(questions[i].text)
-        if (i == 1):
-          score += int(questions[i].text) * 3
-        if (i == 2):
-          score += int(questions[i].text) * 5
-    entity['score'] = score
-    un = soup.find(
-        'div',
-        class_=
-        'text-label-1 dark:text-dark-label-1 break-all text-base font-semibold'
-    )
-    if (un is not None):
-      entity['username'] = un.text
+    easy_solved =0
+    medium_solved=0
+    hard_solved=0
+    if response.status_code == 200:
+      data = json.loads(response.text)
+      easy_solved = int(data["easySolved"])
+      medium_solved=int(data["mediumSolved"])
+      hard_solved=int(data["hardSolved"])
+    entity['score']=easy_solved+medium_solved*3+hard_solved*5
+    entity['username']=k
     participants.append(entity)
+      
   return participants
 
 
