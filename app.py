3b273ecc1cb0d86aa399c32e2ea8ed689536213b
arnavhax <113230128+arnavhax@users.noreply.github.com>
2023-09-15 08:31:48 +0000

    Beautiful soup and requests test

--- app.py
+++ app.py
@@ -1,26 +1,40 @@
 from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup
+
+profileLinks = [
+    "https://leetcode.com/arnavhax/", "https://leetcode.com/Anirudha_Shekdar/"
+]
+
+
+def makeParticipants(profileLinks):
+  participants = []
+  for k in profileLinks:
+    response = requests.get(k)
+    soup = BeautifulSoup(response.content, "html.parser")
+    entity = {}
+
+  participants = [
+      {
+          'username': "Arnav Balpande",
+          'score': 100,
+      },
+      {
+          'username': "Riya Kashikar",
+          'score': 10,
+      },
+  ]
+
+  return participants
+
 
 app = Flask(__name__)
-
-participants = [
-    {
-        'id': 1,
-        'name': "Arnav Balpande",
-        'rank': 1,
-        'score': 100,
-    },
-    {
-        'id': 2,
-        'name': "Riya Kashikar",
-        'rank': 2,
-        'score': 10,
-    },
-]
 
 
 @app.route('/')
 def home():
-  return render_template('home.html', participants=participants)
+  return render_template('home.html',
+                         participants=makeParticipants(profileLinks))
 
 
 if __name__ == "__main__":
